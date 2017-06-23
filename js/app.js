39204cc8569cd8a6e53805f518ea5b07d7b53be0
Jonatas Emidio <jonatasemidio@gmail.com>
2017-06-23 16:42:08 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@
   var config = {
 
     //TODO: Atualizar pelos dados do firebase da conta do devfest ou gdgrio
-    apiKey: "apiKey",
-    authDomain: "authDomain",
-    databaseURL: "databaseURL",
-    projectId: "projectId",
-    storageBucket: "storageBucket",
-    messagingSenderId: "messagingSenderId"
+    apiKey: "AIzaSyBTfT1sRCmXK4PAfq9SmFOE6zXArlfZ7_Y",
+    authDomain: "dfrio-3ab08.firebaseapp.com",
+    databaseURL: "https://dfrio-3ab08.firebaseio.com",
+    projectId: "dfrio-3ab08",
+    storageBucket: "dfrio-3ab08.appspot.com",
+    messagingSenderId: "263686375294"
   };
   firebase.initializeApp(config);
 
